23ad632d2dabcf530d35726d91d3f6b13fd6d01c
sykim0181 <soyeon364@naver.com>
2026-01-10 00:16:54 +0900

    chore: 폰트 설정
    - pretendard 폰트 파일 추가 및 변수 설정

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: "../assets/fonts/PretendardVariable.woff2",
+    },
+  ],
+  variable: "--font-pretendard",
+});
