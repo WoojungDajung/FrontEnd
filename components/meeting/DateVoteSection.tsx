f4ca2b999e1560a52746150140b611c53246ae76
sykim0181 <soyeon364@naver.com>
2026-01-11 16:19:53 +0900

    refactor: 투표 인원 상태 버튼 컴포넌트 분리
    - 장소 투표 섹션에도 사용되서 재사용할 수 있게  분리

--- components/meeting/DateVoteSection.tsx
+++ components/meeting/DateVoteSection.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import PersonIcon from "./PersonIcon";
 import DateVoteCard from "./DateVoteCard";
+import CountButton from "./CountButton";
 
 const DateVoteSection = () => {
-  const currentCount = 0;
-  const totalCount = 0;
-
   return (
     <section className="flex flex-col gap-8">
       <div className="w-full flex justify-between items-center">
         <p className="typo-16-regular text-gray-800 relative left-8">일정</p>
-        <button className="px-8 py-4 rounded-[12px] flex">
-          <PersonIcon />
-          <p className="text-gray-700 typo-14-regular">
-            <span className="text-primary-400">{currentCount}</span>/
-            {totalCount}
-          </p>
-        </button>
+        <CountButton currentCount={0} totalCount={0} onClick={() => {}} />
       </div>
       <DateVoteCard />
     </section>
