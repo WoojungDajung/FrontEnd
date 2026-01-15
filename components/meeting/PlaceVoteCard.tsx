2e576923855f8ab2b7fcfe1dcf0f3a81b125cb20
sykim0181 <soyeon364@naver.com>
2026-01-15 11:05:03 +0900

    feat: 장소 투표 섹션 unregistered 상태일 때

--- components/meeting/PlaceVoteCard.tsx
+++ components/meeting/PlaceVoteCard.tsx
@@ -1,14 +1,30 @@
+import { cn } from "@/utils/cn";
 import Button from "../shared/Button";
 import PlaceIcon from "./PlaceIcon";
 
-const PlaceVoteCard = () => {
+interface PlaceVoteCardProps {
+  disabled?: boolean;
+}
+
+const PlaceVoteCard = ({ disabled }: PlaceVoteCardProps) => {
   return (
     <div className="bg-white border border-gray-100 rounded-[24px] flex flex-col gap-16 items-center py-16">
       <div className="w-310 h-104 flex flex-col gap-4 justify-center items-center">
-        <PlaceIcon />
-        <p className="typo-14-regular text-gray-300">장소를 등록해주세요.</p>
+        <PlaceIcon
+          fill={disabled ? "var(--color-gray-300)" : "var(--color-gray-500)"}
+        />
+        <p
+          className={cn(
+            "typo-14-regular",
+            disabled ? "text-gray-300" : "text-gray-500"
+          )}
+        >
+          장소를 등록해주세요.
+        </p>
       </div>
-      <Button size="Medium" color="White" className="w-310">장소 등록하기</Button>
+      <Button size="Medium" color="White" className="w-310" disabled={disabled}>
+        장소 등록하기
+      </Button>
     </div>
   );
 };
--- components/meeting/PlaceVoteSection.tsx
+++ components/meeting/PlaceVoteSection.tsx
@@ -2,13 +2,16 @@ import CountButton from "./CountButton";
 import PlaceVoteCard from "./PlaceVoteCard";
 
 const PlaceVoteSection = () => {
+  // 프로필 등록 여부 -> TODO: 추후에 수정
+  const isRegistered = false;
+
   return (
     <section className="flex flex-col gap-8">
       <div className="w-full flex justify-between items-center">
         <p className="typo-16-regular text-gray-800 relative left-8">장소</p>
         <CountButton currentCount={0} totalCount={0} onClick={() => {}} />
       </div>
-      <PlaceVoteCard />
+      <PlaceVoteCard disabled={!isRegistered} />
     </section>
   );
 };
