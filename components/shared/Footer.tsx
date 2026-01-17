b48c70f624e41e3f72004a677d94f4cbf53e9da0
sykim0181 <soyeon364@naver.com>
2026-01-17 15:08:41 +0900

    feat: 오류 보고 구글폼 링크 설정

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,13 @@ const Footer = () => {
       <p className="text-center typo-12-regular text-gray-400">
         문제가 발생했나요?
         <br />
-        <Link href={"/"} className="underline">
+        <Link
+          href={
+            "https://docs.google.com/forms/d/e/1FAIpQLSfkUG8QoHKiKoKoiSzVwaWN8GLfYUmvlRmPxqkxsGUXAdwApw/viewform?usp=dialog"
+          }
+          target="_blank"
+          className="underline"
+        >
           오류 신고 및 문의는 여기를 눌러주세요.
         </Link>
       </p>
