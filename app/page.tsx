bf8904566bd26d873ed3e5283ef90afae5f94e5f
sykim0181 <soyeon364@naver.com>
2026-01-20 00:05:33 +0900

    fix: useSearchParams 사용하는 부분 Suspense로 감싸기
    - 관련 자료:
      https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Login from "@/components/home/Login";
 import Swiper from "@/components/home/HeroSwiper";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-32">
         <Swiper />
-        <Login />
+        <Suspense>
+          <Login />
+        </Suspense>
       </div>
     </main>
   );
