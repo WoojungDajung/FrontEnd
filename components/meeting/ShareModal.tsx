6173ae65bc072a19b6252035140c8b715f55a98e
sykim0181 <soyeon364@naver.com>
2026-01-21 15:43:33 +0900

    feat: 카카오톡 공유 기능 구현
    - 카카오톡 sdk 관련 타입 정의
    - 만들어둔 커스텀 템플릿에 맞춰 공유 메시지 구현
    - TODO: 추후 공유 메시지 팀 내 기획 완료 후 수정 필요

--- components/meeting/ShareModal.tsx
+++ components/meeting/ShareModal.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import CloseIcon from "../shared/CloseIcon";
 import KakaoIcon from "../shared/KakaoIcon";
 import Modal from "../shared/Modal";
@@ -11,11 +12,7 @@ interface ShareModalProps {
 
 const ShareModal = ({ open, setOpen }: ShareModalProps) => {
   //TODO: 유효 링크로 변경
-  const link = "";
-
-  const shareOnKakaoTalk = () => {
-    // TODO: 카카오톡 공유 기능 구현
-  };
+  const link = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/1`;
 
   const showMoreShare = () => {
     navigator.share({ url: link });
@@ -25,56 +22,80 @@ const ShareModal = ({ open, setOpen }: ShareModalProps) => {
     navigator.clipboard.writeText(link);
   };
 
+  /* 카카오톡 공유 */
+  const initiateKakao = () => {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+  };
+
+  const shareOnKakaoTalk = () => {
+    window.Kakao.Share.sendCustom({
+      templateId: 128317,
+      templateArgs: {
+        meeting_id: 1,
+        meeting_name: "스터디밥먹으러",
+      },
+    });
+  };
+
   return (
-    <Modal
-      className="pt-8 pb-32"
-      open={open}
-      onOpenChange={(open) => setOpen?.(open)}
-    >
-      {({ close }) => (
-        <>
-          <div className="w-full px-8 flex justify-end">
-            <button className="button" onClick={close}>
-              <CloseIcon />
-            </button>
-          </div>
-          <div className="flex flex-col gap-24">
-            <p className="typo-16-regular text-gray-800 text-center">
-              만날우정, 함께할 친구를 불러보세요!
-            </p>
-            <div className="flex justify-center gap-24">
-              <div className="flex flex-col gap-8 items-center">
-                <button
-                  className="button w-56 h-56 bg-[#FEE500] rounded-full"
-                  onClick={shareOnKakaoTalk}
-                >
-                  <KakaoIcon width={24} height={24} />
-                </button>
-                <p className="typo-12-regular text-gray-500">카카오톡</p>
-              </div>
-              <div className="flex flex-col gap-8 items-center">
-                <button
-                  className="button w-56 h-56 bg-gray-100 rounded-full"
-                  onClick={showMoreShare}
-                >
-                  <MoreIcon width={24} height={24} />
-                </button>
-                <p className="typo-12-regular text-gray-500">더보기</p>
-              </div>
-              <div className="flex flex-col gap-8 items-center">
-                <button
-                  className="button w-56 h-56 bg-gray-100 rounded-full"
-                  onClick={copyLink}
-                >
-                  <CopyIcon width={24} height={24} />
-                </button>
-                <p className="typo-12-regular text-gray-500">링크 복사</p>
+    <>
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.9/kakao.min.js"
+        integrity="sha384-JpLApTkB8lPskhVMhT+m5Ln8aHlnS0bsIexhaak0jOhAkMYedQoVghPfSpjNi9K1"
+        crossOrigin="anonymous"
+        onLoad={initiateKakao}
+      />
+
+      <Modal
+        className="pt-8 pb-32"
+        open={open}
+        onOpenChange={(open) => setOpen?.(open)}
+      >
+        {({ close }) => (
+          <>
+            <div className="w-full px-8 flex justify-end">
+              <button className="button" onClick={close}>
+                <CloseIcon />
+              </button>
+            </div>
+            <div className="flex flex-col gap-24">
+              <p className="typo-16-regular text-gray-800 text-center">
+                만날우정, 함께할 친구를 불러보세요!
+              </p>
+              <div className="flex justify-center gap-24">
+                <div className="flex flex-col gap-8 items-center">
+                  <button
+                    className="button w-56 h-56 bg-[#FEE500] rounded-full"
+                    onClick={shareOnKakaoTalk}
+                  >
+                    <KakaoIcon width={24} height={24} />
+                  </button>
+                  <p className="typo-12-regular text-gray-500">카카오톡</p>
+                </div>
+                <div className="flex flex-col gap-8 items-center">
+                  <button
+                    className="button w-56 h-56 bg-gray-100 rounded-full"
+                    onClick={showMoreShare}
+                  >
+                    <MoreIcon width={24} height={24} />
+                  </button>
+                  <p className="typo-12-regular text-gray-500">더보기</p>
+                </div>
+                <div className="flex flex-col gap-8 items-center">
+                  <button
+                    className="button w-56 h-56 bg-gray-100 rounded-full"
+                    onClick={copyLink}
+                  >
+                    <CopyIcon width={24} height={24} />
+                  </button>
+                  <p className="typo-12-regular text-gray-500">링크 복사</p>
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      )}
-    </Modal>
+          </>
+        )}
+      </Modal>
+    </>
   );
 };
 
