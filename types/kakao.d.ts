607715e988e88a6b740345e2ca48cdfc1c7f969f
sykim0181 <soyeon364@naver.com>
2026-01-22 11:40:57 +0900

    feat: 장소 정보 드로워 카카오맵 구현
    - 관련 sdk 스크립트 추가
    - 드로워가 visible해질 때(= mapContainer div가 마운트되었을 때) 맵을
      임베딩하도록 처리
    - 클릭 시 새 페이지로 카카오맵 페이지 열리도록 처리

--- types/kakao.d.ts
+++ types/kakao.d.ts
@@ -3,6 +3,8 @@ export {};
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    Kakao: any;
+    Kakao: any; // t1.kakaocdn.net/kakao_js_sdk (공유 포함)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any; // dapi.kakao.com/v2/maps/sdk.js
   }
 }
