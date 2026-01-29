06145a70b0d6aefc186520dcd6447e17f6f2c528
sykim0181 <soyeon364@naver.com>
2026-01-29 18:16:15 +0900

    feat: 일정 확정 시 UI 구현
    - 서버사이드에서 일정 확정 여부 확인
    - 결과 모달이 처음부터 보여지도록 구현
      - 현재 모달이 보여지려면 div#modal이 마운트되어야 하기 때문에 모달이
        처음엔 보이지 않다가 잠시 후 보이는 문제가 있음

---
+++ components/meeting/MeetingSettledSection.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Button from "../shared/Button";
+import { useEffect, useState } from "react";
+import MeetingResultModal from "./MeetingResultModal";
+
+const MeetingSettledSection = () => {
+  const [resultModalOpen, setResultModalOpen] = useState(false);
+
+  // 모달이 열려져 있도록 하기 위한 작업
+  useEffect(() => {
+    // resultModalOpen 초기값을 true로 하는 경우 modal div가 마운트되지 않아서 무시됨
+    // 그래서 대신 마운트 이후 값을 true로 바꾸어 모달이 나타나도록 함
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setResultModalOpen(true);
+  }, []);
+
+  return (
+    <section className="flex flex-col gap-24">
+      <div className="pt-32 pb-24 rounded-[24px] flex flex-col items-center gap-16 border border-gray-100 bg-primary-25">
+        <div className="flex flex-col gap-16 items-center">
+          <Image
+            src={"/images/Celebration.svg"}
+            alt="폭죽"
+            width={56}
+            height={56}
+          />
+          <p className="typo-18-semibold text-gray-800 text-center">
+            우리의 약속이 완성되었어요!
+          </p>
+        </div>
+        <Button
+          size="Medium"
+          color="Primary"
+          onClick={() => setResultModalOpen(true)}
+        >
+          확인하기
+        </Button>
+      </div>
+      <p className="typo-14-regular text-gray-400 text-center">
+        다시 정하고 싶다면 아래에서 마감일을 수정해보세요.
+      </p>
+
+      {/* 확정 결과 모달 */}
+      <MeetingResultModal open={resultModalOpen} setOpen={setResultModalOpen} />
+    </section>
+  );
+};
+
+export default MeetingSettledSection;
