66ef25bd878ca735e50f239dfb7ef615cdc4b10e
sykim0181 <soyeon364@naver.com>
2026-01-30 16:46:17 +0900

    feat: 에러 페이지 ui 구현

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,7 +1,32 @@
+import Button from "@/components/shared/Button";
+import Image from "next/image";
+
 const Page = () => {
   return (
-    <></>
-  )
-}
+    <div className="flex flex-col gap-96 items-center">
+      <div className="flex flex-col gap-24 items-center">
+        <Image
+          src={"/images/Error_Illustration.svg"}
+          alt="울고 있는 이미지"
+          width={160}
+          height={160}
+        />
+        <div className="flex flex-col gap-8">
+          <p className="typo-20-bold text-gray-800 text-center">
+            일시적인 오류가 발생했어요...
+          </p>
+          <p className="typo-16-regular text-gray-500 text-center">
+            네트워크를 연결해 주시고
+            <br />
+            잠시 후에 다시 시도해 주세요.
+          </p>
+        </div>
+      </div>
+      <Button size="Medium" color="Primary">
+        다시 시도하기
+      </Button>
+    </div>
+  );
+};
 
 export default Page;
