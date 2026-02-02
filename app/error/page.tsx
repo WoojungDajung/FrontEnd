903f3131f5df159e1d26c85968a7981b70e3afa4
sykim0181 <soyeon364@naver.com>
2026-02-02 16:27:49 +0900

    feat: 404 페이지 개발
    - error 페이지에서 사용되던 UI 분리하여 재사용 (버튼 제외)

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,28 +1,20 @@
-import Image from "next/image";
 import { Suspense } from "react";
 import RetryButton from "./RetryButton";
+import ErrorUI from "@/components/shared/ErrorUI";
 
 const Page = () => {
   return (
     <div className="flex flex-col gap-96 items-center">
-      <div className="flex flex-col gap-24 items-center">
-        <Image
-          src={"/images/Error_Illustration.svg"}
-          alt="울고 있는 이미지"
-          width={160}
-          height={160}
-        />
-        <div className="flex flex-col gap-8">
-          <p className="typo-20-bold text-gray-800 text-center">
-            일시적인 오류가 발생했어요...
-          </p>
-          <p className="typo-16-regular text-gray-500 text-center">
+      <ErrorUI
+        mainMessage={"일시적인 오류가 발생했어요..."}
+        subMessage={
+          <>
             네트워크를 연결해 주시고
             <br />
             잠시 후에 다시 시도해 주세요.
-          </p>
-        </div>
-      </div>
+          </>
+        }
+      />
       <Suspense>
         <RetryButton />
       </Suspense>
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import ErrorUI from "@/components/shared/ErrorUI";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-96 items-center">
+      <ErrorUI mainMessage={"존재하지 않는 페이지입니다."} />
+    </div>
+  );
+}
