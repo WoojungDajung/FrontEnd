fe087ad6e88dcde8cfe0adb324d26ebde97892b6
sykim0181 <soyeon364@naver.com>
2026-02-03 21:37:30 +0900

    feat: logout api 요청 함수 작성

--- api/auth.ts
+++ api/auth.ts
@@ -25,3 +25,22 @@ export async function reissueToken(
   const newAccessToken = resBody.data as string;
   return newAccessToken;
 }
+
+// 로그아웃
+export async function logout(refreshToken: string): Promise<void> {
+  const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/logout?refreshToken=${refreshToken}`;
+  const res = await fetch(url, {
+    method: "POST",
+  });
+
+  const resBody = await res.json();
+  const { status_code, message } = resBody;
+
+  if (!res.ok || status_code !== 200) {
+    if (status_code === 400) {
+      // Refresh Token 존재하지 않음
+    }
+    throw new Error(`${status_code}: ${message}`);
+  }
+  return;
+}
