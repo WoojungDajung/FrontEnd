74f314d56f4fde452403af4299690de1392a84e1
sykim0181 <soyeon364@naver.com>
2026-02-15 11:33:53 +0900

    Revert "feat: 로그인 상태인 경우 메인 화면으로 이동 처리"
    This reverts commit a684301ef0e8c31b068ac8ee444c9e4149f30abc.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 import Login from "@/components/home/Login";
 import Swiper from "@/components/home/HeroSwiper";
 import { Suspense } from "react";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("access-token");
-  if (!accessToken) {
-    redirect("/appointments");
-  }
-
+export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-32">
