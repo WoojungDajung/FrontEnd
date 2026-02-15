6c1a5b08e2cdb9d69cc537e12bd092c9265ecfd2
sykim0181 <soyeon364@naver.com>
2026-02-15 12:26:29 +0900

    feat: 약속 페이지 홈버튼 추가

--- app/meeting/[meetingId]/page.tsx
+++ app/meeting/[meetingId]/page.tsx
@@ -1,5 +1,6 @@
 import { getAppointment } from "@/api/appointment";
 import DateVoteSection from "@/components/meeting/DateVoteSection";
+import HomeButton from "@/components/meeting/HomeButton";
 import MeetingInfoSection from "@/components/meeting/MeetingInfoSection";
 import MeetingSettledSection from "@/components/meeting/MeetingSettledSection";
 import PlaceVoteSection from "@/components/meeting/PlaceVoteSection";
@@ -11,12 +12,12 @@ import { cookies } from "next/headers";
 import { notFound, redirect } from "next/navigation";
 
 async function getAppointmentServer(appointmentId: string) {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
   return getAppointment(appointmentId, {
     headers: {
-      cookie: cookieStore.toString()
-    }
-  })
+      cookie: cookieStore.toString(),
+    },
+  });
 }
 
 const Page = async ({ params }: { params: Promise<{ meetingId: string }> }) => {
@@ -29,7 +30,7 @@ const Page = async ({ params }: { params: Promise<{ meetingId: string }> }) => {
   try {
     appointmentInfo = await queryClient.fetchQuery({
       queryKey: ["appointment", meetingId],
-      queryFn: async ({ queryKey }) => getAppointmentServer(queryKey[1])
+      queryFn: async ({ queryKey }) => getAppointmentServer(queryKey[1]),
     });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
@@ -43,24 +44,28 @@ const Page = async ({ params }: { params: Promise<{ meetingId: string }> }) => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className="flex flex-col gap-24">
-        {isSettled && appointmentInfo.confirmedResult !== null && (
-          <MeetingSettledSection
-            appointment={appointmentInfo.appointment}
-            appointmentUserCount={appointmentInfo.appointmentUserList.length}
-            result={appointmentInfo.confirmedResult}
-          />
-        )}
-        <AppointmentPageProvider>
-          <div className="flex flex-col gap-16">
-            <MeetingInfoSection appointmentId={meetingId} />
-            <DateVoteSection appointmentId={meetingId} canVote={!isSettled} />
-            <PlaceVoteSection
-              appointmentId={meetingId}
-              isAppointmentSettled={isSettled}
+      <main className="flex flex-col gap-4">
+        <HomeButton />
+
+        <div className="flex flex-col gap-24">
+          {isSettled && appointmentInfo.confirmedResult !== null && (
+            <MeetingSettledSection
+              appointment={appointmentInfo.appointment}
+              appointmentUserCount={appointmentInfo.appointmentUserList.length}
+              result={appointmentInfo.confirmedResult}
             />
-          </div>
-        </AppointmentPageProvider>
+          )}
+          <AppointmentPageProvider>
+            <div className="flex flex-col gap-16">
+              <MeetingInfoSection appointmentId={meetingId} />
+              <DateVoteSection appointmentId={meetingId} canVote={!isSettled} />
+              <PlaceVoteSection
+                appointmentId={meetingId}
+                isAppointmentSettled={isSettled}
+              />
+            </div>
+          </AppointmentPageProvider>
+        </div>
       </main>
     </HydrationBoundary>
   );
