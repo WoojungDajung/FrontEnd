3b1185eec5ee48e03b69e2c61dfaaca70c8c9870
sykim0181 <soyeon364@naver.com>
2026-02-16 21:10:19 +0900

    fix: 약속 삭제 시 참여 약속 목록 쿼리 invalidate 처리

--- hooks/useDeleteAppointment.ts
+++ hooks/useDeleteAppointment.ts
@@ -1,9 +1,13 @@
 import { deleteAppointment } from "@/api/appointment";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const useDeleteAppointment = (appointmentId: string) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: () => deleteAppointment(appointmentId),
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["member-appointments"] }),
   });
 };
 
