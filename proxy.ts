8e25528e131aeac9eb620d36389fead0a1fd7a1c
sykim0181 <soyeon364@naver.com>
2026-02-16 16:42:06 +0900

    refactor: setup-meeting 페이지 삭제
    - 해당 페이지의 기능이 모두 /appointments 페이지로 흡수됨에 따라 관련
      컴포넌트 삭제 및 파일 위치 이동

--- proxy.ts
+++ proxy.ts
@@ -2,10 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/setup-meeting")) {
-    return NextResponse.redirect(new URL("/appointments", request.url));
-  }
-
   // 비로그인 상태 -> 접근 X
   const athenticated = await isAuthenticated(request);
   if (!athenticated) {
@@ -16,7 +12,7 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/setup-meeting", "/appointments", "/appointment/:path*"],
+  matcher: ["/appointments", "/appointment/:path*"],
 };
 
 async function isAuthenticated(request: NextRequest): Promise<boolean> {
