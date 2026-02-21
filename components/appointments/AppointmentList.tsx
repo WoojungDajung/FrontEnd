393a1561fe65ef04ff76a318f36ae15d7a57b6d7
sykim0181 <soyeon364@naver.com>
2026-02-21 17:54:20 +0900

    style: 참여 목록 로딩 스피너 위치 변경

--- components/appointments/AppointmentList.tsx
+++ components/appointments/AppointmentList.tsx
@@ -9,7 +9,7 @@ const AppointmentList = () => {
 
   if (data === undefined) {
     return (
-      <div className="w-full flex justify-center">
+      <div className="w-full h-340 flex justify-center items-center">
         <LoadingSpinner open size={25} />
       </div>
     );
