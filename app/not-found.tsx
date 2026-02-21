936dcbee672b8b9c7e09b017a2de8bcd09514456
sykim0181 <soyeon364@naver.com>
2026-02-21 16:39:58 +0900

    feat: not found 페이지에 홈으로 돌아가기 버튼 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,16 @@
 import ErrorUI from "@/components/shared/ErrorUI";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-96 items-center justify-center pt-90">
       <ErrorUI mainMessage={"존재하지 않는 페이지입니다."} />
+      <Link
+        href="/"
+        className="button button--md button--primary typo-18-semibold"
+      >
+        홈으로 돌아가기
+      </Link>
     </div>
   );
 }
