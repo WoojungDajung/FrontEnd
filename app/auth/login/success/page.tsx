801ad62acb83b64234349cbd0a8e168726598816
sykim0181 <soyeon364@naver.com>
2026-03-12 13:01:47 +0900

    style: spinner 사이즈 수정

--- app/auth/login/success/page.tsx
+++ app/auth/login/success/page.tsx
@@ -5,7 +5,7 @@ import LoginSuccessHandler from "./LoginSuccessHandler";
 const Page = () => {
   return (
     <div className="fixed left-0 top-0 w-dvw h-dvh grid place-content-center">
-      <LoadingSpinner open size={50} />
+      <LoadingSpinner open size={25} />
 
       <Suspense>
         <LoginSuccessHandler />
